375de696412a376771b5c9478626fa7e9da271fc
Ioan1234 <101118809+Ioan1234@users.noreply.github.com>
2022-11-20 16:01:08 +0200

    Add files via upload

--- proiect_script.js
+++ proiect_script.js
@@ -1,16 +1,4 @@
-function myFunction1() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
-  function myFunction2() {
+function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
