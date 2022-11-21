d8ec0740b3f053d37af83060a3a36ed2f0a673b2
Andrei Cirlan <90861082+AndreiCirlan@users.noreply.github.com>
2022-11-21 12:25:07 +0200

    Add files via upload

--- proiect_script.js
+++ proiect_script.js
@@ -6,3 +6,9 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  var modal = document.getElementById('id01');
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
