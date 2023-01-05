0b6fdd0aee6de630cdfbd38846f3fb779189c9aa
Ioan1234 <101118809+Ioan1234@users.noreply.github.com>
2023-01-05 23:08:55 +0200

    Add files via upload

--- proiect_script.js
+++ proiect_script.js
@@ -1,14 +1,33 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset > 90) {
+    document.querySelector(".nav-bar").classList.add("nav-sticky");
+    document.querySelector(".page-header").style.marginTop = "73px";
+  } else {
+    document.querySelector(".nav-bar").classList.remove("nav-sticky");
+    document.querySelector(".page-header").style.marginTop = "0";
+  }
+});
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+var loader = function () {
+  setTimeout(function () {
+    if (document.querySelectorAll('#loader').length > 0) {
+      document.querySelector('#loader').classList.remove('show');
     }
-  }
-  var modal = document.getElementById('id01');
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
+  }, 1);
+};
+loader();
